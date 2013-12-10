8c86bbd90e6fbf8c131dc640f36dd6c49eaad63e
alexanderva <alexanderva@gmail.com>
2013-12-10 20:20:52 +0200

    Initial commit for ex3

---
+++ A14 Ex03 AlexanderVainshtein 312717218 DanielShmuglin 305870636/FacebookApp.Model/Translator/TranslatorFactory.cs
@@ -0,0 +1,39 @@
+namespace Ex2.FacebookApp.Model.Translator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class TranslatorFactory
+    {
+        public static ITranslator Create(eTranslatorType i_Type, eTranslationLang i_TargetLanguageCode, IEnumerable<eTranslationLang> i_SkippedLanguageCodes = null)
+        {
+            switch (i_Type)
+            {
+                case eTranslatorType.Dummy:
+                    return new DummyTranslator();
+                case eTranslatorType.Bing:
+                    return new BingTranslator(i_TargetLanguageCode.ToString(), i_SkippedLanguageCodes == null ? null : i_SkippedLanguageCodes.Cast<string>());
+                case eTranslatorType.Base64:
+                    return new Base64Translator();
+                default:
+                    throw new ArgumentException("Unsupported translator type");
+            }
+        }
+    }
+
+    public enum eTranslatorType
+    { 
+        Dummy,
+        Bing,
+        Base64
+    }
+
+    public enum eTranslationLang
+    { 
+        ru,
+        en,
+        he
+    }
+}
---
+++ A14 Ex03 AlexanderVainshtein 312717218 DanielShmuglin 305870636/readme.txt
@@ -0,0 +1,9 @@
+==========================Exercise Checking Report========================== 
+Exercise No...........: 03
+First Student Details.: 312717218 - Alexander Vainshtein
+Second Student Details: 305870636 - Daniel Shmuglin
+Delivery Date.........: 17 - Dec - 2013
+Delivered In Delay....: No
+Delay Reason..........: 
+Comments..............: 
+=======================End Exercise Checking Report=========================
